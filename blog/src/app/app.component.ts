2a5c4135e87d24fdd9d36e9380ed26a75e29d404
Rohit Ranjan <rohitgns54321@gmail.com>
2023-05-28 12:00:04 +0530

    style binding

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -46,5 +46,56 @@ export class AppComponent {
 
 
   //LEC 14
+  // name="Rohit";
+  // // disable = true;
+  // disable = false;\
+
+
+  //lec 15
+
+  // show=true;
+  // show="yes";
+
+  //LEC 16
+  // color='blue'
+
+
+  //LEC 17
+  // color='d';
+
+
+  // lec 18
+  // users=['Rohit','Aman','Madan','sadan']
+  // details=[
+  //   {name:'Rohit',email:'aa@gmail.com',phone:'1111'},
+  //   {name:'Aman',email:'ll@gmail.com',phone:'12321'},
+  //   {name:'Madan',email:'vv@gmail.com',phone:'1114231'},
+  //   {name:'sadan',email:'dd@gmail.com',phone:'432321'},
+  // ]
+
+
+
+  //lec 19
+
+  // users=[
+  //   {name:'Rohit',email:'aa@gmail.com',phone:'1111',accounts:['facebook','gmail','lindin']},
+  //   {name:'Aman',email:'ll@gmail.com',phone:'12321',accounts:['gfgd','yahoo','lindin']},
+  //   {name:'Madan',email:'vv@gmail.com',phone:'1114231',accounts:['facgdgebook','hotmail','youtobe']},
+  //   {name:'sadan',email:'dd@gmail.com',phone:'432321',accounts:['gg','snapchat','insta']},
+  // ]
+
+
+
+  //LEC 20
+  // color="green";
+  // bgColor="red";
+  // updateColor(){
+  //   this.color="blue";
+  //   this.bgColor="pink";
+  // }
+
+
+
+  //lec 21
   
 }
